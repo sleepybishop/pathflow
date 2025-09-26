b7a6b5ac1de8526e90404bf389996314905cf81d
joseph calderon <calderon.joe@gmail.com>
2025-09-26 14:17:16 -0700

    fix qnorm

--- pathflow.c
+++ pathflow.c
@@ -15,17 +15,27 @@ typedef struct {
     size_t m; // packets allocated to this link
 } path_t;
 
-float qnorm(float P)
+/**
+ * return zscore given probability of success
+ **/
+
+float qnorm(size_t P)
 {
-    float z_c[] = {0.000, 0.025, 0.050, 0.075, 0.100, 0.126, 0.151, 0.176, 0.202, 0.228, 0.253, 0.279, 0.305,
-                   0.332, 0.358, 0.385, 0.412, 0.440, 0.468, 0.496, 0.524, 0.553, 0.583, 0.613, 0.643, 0.674,
-                   0.706, 0.739, 0.772, 0.806, 0.842, 0.878, 0.915, 0.954, 0.994, 1.036, 1.080, 1.126, 1.175,
-                   1.227, 1.282, 1.341, 1.405, 1.476, 1.555, 1.645, 1.751, 1.881, 2.054, 2.326};
-    int Pi = (int)(P * 100.0);
-    return (Pi >= 50) ? z_c[Pi - 50] : -z_c[50 - Pi - 1];
+    if (P == 50)
+        return 0.0f;
+    if (P <= 0)
+        return -3.090f;
+    if (P >= 100)
+        return 3.090f;
+    static const float z_c[] = {0.025f, 0.050f, 0.075f, 0.100f, 0.126f, 0.151f, 0.176f, 0.202f, 0.228f, 0.253f,
+                                0.279f, 0.305f, 0.332f, 0.358f, 0.385f, 0.412f, 0.440f, 0.468f, 0.496f, 0.524f,
+                                0.553f, 0.583f, 0.613f, 0.643f, 0.674f, 0.706f, 0.739f, 0.772f, 0.806f, 0.842f,
+                                0.878f, 0.915f, 0.954f, 0.994f, 1.036f, 1.080f, 1.126f, 1.175f, 1.227f, 1.282f,
+                                1.341f, 1.405f, 1.476f, 1.555f, 1.645f, 1.751f, 1.881f, 2.054f, 2.326f};
+    return (P > 50) ? z_c[P - 51] : -z_c[49 - P];
 }
 
-size_t psi(float Ps, size_t m, float p)
+size_t psi(size_t Ps, size_t m, float p)
 {
     float mp = m * p;
     size_t n = ceil((qnorm(Ps) * sqrt(mp) + mp) / (1 - p));
