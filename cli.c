55653c0a253b01e9bf609a97844dafaf2144cc8a
joseph calderon <calderon.joe@gmail.com>
2026-05-30 07:38:20 -0700

    integrate ewma path state smoothing

--- cli.c
+++ cli.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include "pathflow.h"
 
 #define CLAMP(x, a, b) ((x < (a)) ? (a) : ((x > (b)) ? (b) : (x)))
 
-int main() {
+int main()
+{
     path_t path[16] = {0};
     size_t N = 0, K = 0;
     float Ps_f = 0.0, deadline = 60.0;
 
     FILE *in = fopen("problem.txt", "r");
-    if (!in) exit(-1);
+    if (!in)
+        exit(-1);
 
     fscanf(in, "N: %zu\n", &N);
     fscanf(in, "K: %zu\n", &K);
@@ -21,15 +24,28 @@ int main() {
     Ps_f = CLAMP(Ps_f, 0.01f, 0.99f);
     size_t Ps = (size_t)roundf(Ps_f * 100.0f);
     printf("N: %zu, K: %zu, Ps: %.2f\n", N, K, Ps_f);
+    path_state_t states[16] = {0};
+    float alpha = 0.25f;
 
     size_t i = 0;
-    for (i = 0; i < N && !feof(in); i++) {
-        int got = fscanf(in, "%f %f %f %zu\n", &path[i].b, &path[i].l, &path[i].p, &path[i].q);
-        if (got != 4) break;
-        printf("path[%zu] %f %f %f %zu\n", i, path[i].b, path[i].l, path[i].p, path[i].q);
+    for (i = 0; i < N; i++) {
+        float raw_b, raw_l, raw_p;
+        size_t raw_q;
+        int got = fscanf(in, "%f %f %f %zu\n", &raw_b, &raw_l, &raw_p, &raw_q);
+        if (got != 4)
+            break;
+        raw_p = CLAMP(raw_p, 0.0f, 0.99f);
+
+        pathflow_update_state(&states[i], raw_b, raw_l, raw_p, raw_q, alpha);
+
+        path[i].b = states[i].b_ewma;
+        path[i].l = states[i].l_ewma;
+        path[i].p = states[i].p_ewma;
+        path[i].q = states[i].q_ewma;
     }
     fclose(in);
-    if (i != N) exit(-1);
+    if (i != N)
+        exit(-1);
 
     float total_time = pathflow_optimize(N, K, path, deadline, Ps);
     printf("estimated transfer time: %.2fs\n", total_time);
@@ -38,7 +54,9 @@ int main() {
     for (size_t j = 0; j < N; j++) {
         alloc += path[j].m;
         extra += path[j].x;
-        printf("m[%2zu]: %5zu + %5zu  [ %6.0f | %6.3f | %6.3f ] -> %8.3f\n", j, path[j].m, path[j].x - path[j].m, path[j].b, path[j].l, path[j].p, path[j].t);
+
+        printf("m[%2zu]: %5zu + %5zu  [ %6.0f | %6.3f | %6.3f ] -> %8.3f\n", j, (size_t)path[j].m, path[j].x - path[j].m, path[j].b,
+               path[j].l, path[j].p, path[j].t);
     }
     printf("m[xx]: %5zu   %5zu\n", alloc, extra);
 
--- pathflow.h
+++ pathflow.h
@@ -13,6 +13,15 @@ typedef struct {
     float t;  // estimated transfer time for this link
 } path_t;
 
+typedef struct {
+    float b_ewma;
+    float l_ewma;
+    float p_ewma;
+    size_t q_ewma;
+    int initialized;
+} path_state_t;
+
+void pathflow_update_state(path_state_t *state, float b, float l, float p, size_t q, float alpha);
 float pathflow_optimize(size_t N, size_t K, path_t *path, float penalty_weight, size_t Ps);
 
 #endif // PATHFLOW_H
